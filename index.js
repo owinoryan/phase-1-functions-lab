41b7508ed474c24c5b3e29dc0415e0665175c3b2
ryanowino <owinoryan@gmail.com>
2024-07-03 23:51:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(pickupLocation) {
+/*function distanceFromHqInBlocks(pickupLocation) {
     const hqLocation = 42;
     const distance = Math.abs(pickupLocation - hqLocation);
   
@@ -55,4 +55,46 @@ function calculatesFarePrice(start, destination) {
   module.exports = {
     calculatesFarePrice
   };
+  */
+
+  v
+  // Function to calculate the distance from HQ in blocks
+  function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    const distance = Math.abs(pickupLocation - hqLocation);
+    return distance;
+  }
   
+  // Function to calculate the distance from HQ in feet
+   function distanceFromHqInFeet(pickupLocation) {
+    const blockInFeet = 264;
+    const hqLocation = 42;
+    const distanceInBlocks = Math.abs(pickupLocation - hqLocation);
+    const distanceInFeet = distanceInBlocks * blockInFeet;
+    return distanceInFeet;
+  }
+  
+  // Function to calculate the distance traveled in feet
+   function distanceTravelledInFeet(start, destination) {
+    const blockInFeet = 264;
+    const distanceInBlocks = Math.abs(destination - start);
+    const distanceInFeet = distanceInBlocks * blockInFeet;
+    return distanceInFeet;
+  }
+  
+  // Function to calculate the fare price
+   function calculatesFarePrice(start, destination) {
+    const blockInFeet = 264;
+    const distanceInFeet = Math.abs(destination - start) * blockInFeet;
+  
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      const farePrice = (distanceInFeet - 400) * 0.02;
+      return parseFloat(farePrice.toFixed(2));
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
--- test/indexTest.js
+++ test/indexTest.js
@@ -29,19 +29,7 @@ describe('index.js', function() {
     });
   });
 
-  describe('distanceTravelledInFeet()', function() {
-    it('returns the distance travelled in feet', function() {
-      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
-    });
-
-    it('returns a distance in feet', function() {
-      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
-    });
-
-    it('returns distance when destination is below distance', function() {
-      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
-    });
-  });
+  
 
   describe('calculatesFarePrice(start, destination)', function() {
     it('gives customers a free sample', function() {
